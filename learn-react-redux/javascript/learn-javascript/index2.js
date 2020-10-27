7be9993ab01d323de06623fbceb9b891f0125251
juliaveronica02 <juliavero2206@gmail.com>
2020-10-27 09:45:55 +0700

    done

--- learn-react-redux/javascript/learn-javascript/index2.js
+++ learn-react-redux/javascript/learn-javascript/index2.js
@@ -30,3 +30,37 @@ let human = {
     // parameter.
     // my job is facebook.
 }
+console.log(human.name); // julia.
+console.log(human.live[1]); // a.
+let petName = human.pet.name // blacky.
+
+let building = {
+    name: 'batam pos',
+    floor: 21,
+    printNameAndFloor: function () {
+        const name = this.name;
+        const floor = this.floor;
+        console.log(`${name} building have ${floor}`)
+    },
+    printOther: otherInfo => console.log(otherInfo),
+    printSecond:(otherInfo)=>{
+        console.log(otherInfo)
+        console.error('danger')
+    }
+}
+//json.stringify make JSON(javascript object notation)/ javascript object to string
+let jsonString = JSON.stringify(building)
+// result =>"{"name":"Batam Pos", "Floor":21};
+// it will get value like string, integer, boolean and float.
+
+//JSON.Parse make JSON berbentuk string kembali ke asalnya/ke object
+let jsonParse = JSON.parse(jsonString)
+console.log(jsonParse)
+// run: JSON.parse(jsonString);
+// result.
+/*
+{
+    "name":"Batam Pos",
+    "floor": "21"
+}
+*/
